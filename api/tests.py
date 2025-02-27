92b70f2354134b9188e1f3baf63227c83b0b5a0a
rigifdaga <rfdauliaa@gmail.com>
2025-02-27 15:57:27 +0700

    [RED] Add failing test for get produk

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from api.models import Produk, KategoriProduk
 
-# Create your tests here.
+class ProdukAPITest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.kategori = KategoriProduk.objects.create(nama="Elektronik")
+
+        self.produk_dengan_foto = Produk.objects.create(
+            nama="Laptop",
+            foto="https://example.com/laptop.jpg",
+            harga_modal=5000000,
+            harga_jual=7000000,
+            stok=10,
+            satuan="Pcs",
+            kategori=self.kategori
+        )
+
+        self.produk_tanpa_foto = Produk.objects.create(
+            nama="Keyboard",
+            foto=None,
+            harga_modal=300000,
+            harga_jual=500000,
+            stok=20,
+            satuan="Pcs",
+            kategori=self.kategori
+        )
+
+    def test_get_produk_success(self):
+        response = self.client.get("/api/produk")
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertEqual(len(data), 2)
+
+        self.assertEqual(data[0]["id"], self.produk_dengan_foto.id)
+        self.assertEqual(data[0]["nama"], "Laptop")
+        self.assertEqual(data[0]["foto"], "https://example.com/laptop.jpg")
+        self.assertEqual(data[0]["harga_modal"], float(self.produk_dengan_foto.harga_modal))
+        self.assertEqual(data[0]["harga_jual"], float(self.produk_dengan_foto.harga_jual))
+        self.assertEqual(data[0]["stok"], float(self.produk_dengan_foto.stok))
+        self.assertEqual(data[0]["satuan"], self.produk_dengan_foto.satuan)
+        self.assertEqual(data[0]["kategori"], "Elektronik")
+
+        self.assertEqual(data[1]["id"], self.produk_tanpa_foto.id)
+        self.assertEqual(data[1]["nama"], "Keyboard")
+        self.assertIsNone(data[1]["foto"])
+        self.assertEqual(data[1]["harga_modal"], float(self.produk_tanpa_foto.harga_modal))
+        self.assertEqual(data[1]["harga_jual"], float(self.produk_tanpa_foto.harga_jual))
+        self.assertEqual(data[1]["stok"], float(self.produk_tanpa_foto.stok))
+        self.assertEqual(data[1]["satuan"], self.produk_tanpa_foto.satuan)
+        self.assertEqual(data[1]["kategori"], "Elektronik")
+
+    def test_get_produk_not_found(self):
+        Produk.objects.all().delete()
+        response = self.client.get("/api/produk")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
+
+    def test_get_produk_invalid_url(self):
+        response = self.client.get("/api/produk-salah")
+        self.assertEqual(response.status_code, 404)
--- backend/urls.py
+++ backend/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
-from api.views import router
-
-api = NinjaAPI()
-api.add_router("", router)
+from api.views import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
