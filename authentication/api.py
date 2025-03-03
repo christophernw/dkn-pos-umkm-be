552278c24f507f17be855329d3545e4e1fae37f9
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-03-04 01:29:52 +0700

    [GREEN] TEST: add auth api.py method

--- authentication/api.py
+++ authentication/api.py
@@ -1,22 +1,82 @@
+from authentication.schemas import LoginSchema, RegisterSchema, RefreshSchema
+from ninja import Router
+from ninja.throttling import AnonRateThrottle
+from ninja_jwt.tokens import RefreshToken
+from ninja_jwt.exceptions import TokenError
+from ninja.errors import HttpError
 from django.contrib.auth.models import User
-from ninja import Router, Schema
-from django.contrib.auth.hashers import make_password
+from ninja_jwt.authentication import JWTAuth
 
 router = Router()
 
-class RegisterSchema(Schema):
-    username: str
-    password: str
-    email: str
+
+@router.post("/login", throttle=AnonRateThrottle(rate="10/h"))
+def login(request, data: LoginSchema):
+    try:
+        user = User.objects.get(username=data.phone_number)
+        if not user.check_password(data.password):
+            raise HttpError(404, "Pengguna tidak terdaftar atau kata sandi salah")
+
+        refresh = RefreshToken.for_user(user)
+        return {
+            "message": "Login berhasil",
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }
+
+    except User.DoesNotExist:
+        raise HttpError(404, "Pengguna tidak terdaftar atau kata sandi salah")
+
 
 @router.post("/register")
 def register(request, data: RegisterSchema):
-    if User.objects.filter(username=data.username).exists():
-        return {"error": "Username already exists"}
-    
-    user = User.objects.create(
-        username=data.username,
-        email=data.email,
-        password=make_password(data.password)  # Hash the password
-    )
-    return {"message": "User registered successfully", "user_id": user.id}
+    try:
+        if User.objects.filter(username=data.phone_number).exists():
+            raise HttpError(400, "Pengguna sudah terdaftar")
+
+        user = User.objects.create_user(
+            username=data.phone_number,
+            password=data.password,
+            first_name=data.first_name,
+            last_name=data.last_name,
+            is_staff=True
+        )
+        refresh = RefreshToken.for_user(user)
+        return {
+            "message": "Akun berhasil dibuat",
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }
+    except Exception as e:
+        raise HttpError(400, str(e))
+
+
+@router.post("/refresh")
+def refresh(request, data: RefreshSchema):
+    try:
+        refresh = RefreshToken(data.refresh)
+        new_access_token = str(refresh.access_token)
+
+        user_id = refresh.payload.get("user_id")
+        if not User.objects.filter(id=user_id).exists():
+            raise HttpError(401, "Pengguna tidak ditemukan atau token tidak valid")
+
+        return {"access": new_access_token}
+    except TokenError:
+        raise HttpError(401, "Token invalid atau telah kadaluarsa")
+
+
+@router.post("/validate", auth=JWTAuth())
+def validate(request):
+    return {"message": "Token valid"}
+
+
+@router.get("/me", auth=JWTAuth())
+def get_user_by_token(request):
+    user = request.auth
+    return {
+        "id": user.id,
+        "phone_number": user.username,
+        "first_name": user.first_name,
+        "last_name": user.last_name
+    }
