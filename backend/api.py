640d0c7dce866d1aecafcf5b71522acd82113542
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2025-03-04 22:56:18 +0700

    [RED] Add test for search produk feature

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,7 @@
 from ninja import NinjaAPI
 from authentication.api import router as auth_router
+from search.api import router as search_router
 
 api = NinjaAPI()
 api.add_router("/auth/", auth_router)
+api.add_router("/produk/", search_router)
---
+++ search/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import KategoriProduk, Produk
+
+admin.site.register(KategoriProduk)
+admin.site.register(Produk)
