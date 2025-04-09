9f3ec7a4a5267e92f439f454995b244131b3fe80
christophernw <christophernathanael10@gmail.com>
2025-04-09 22:39:53 +0700

    [REFACTOR] update user model and migration files to enhance role management and remove redundant fields

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,31 @@
 from datetime import timedelta
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin
 from django.utils.timezone import now
+from django.contrib.auth.hashers import make_password
+from django.apps import apps
+from django.contrib.auth.base_user import AbstractBaseUser
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
 
 class Toko(models.Model):
     # Initially just a simple model to link users
@@ -12,7 +35,17 @@ class Toko(models.Model):
         owner = self.users.filter(role="Pemilik").first()
         return f"Toko {self.id} - {owner.username if owner else 'No owner'}"
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
+    # User fields
+    username = models.CharField(max_length=254, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    
+    # Additional fields required for Django auth system
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=now)
+    
+    # Custom fields
     ROLE_CHOICES = [
         ("Pemilik", "Pemilik"), 
         ("Administrator", "Administrator"), 
@@ -22,6 +55,13 @@ class User(AbstractUser):
     
     # Add foreign key to Toko
     toko = models.ForeignKey(Toko, on_delete=models.SET_NULL, null=True, blank=True, related_name='users')
+    
+    # Required fields
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    # User manager
+    objects = UserManager()
 
 class Invitation(models.Model):
     email = models.EmailField(unique=True)
