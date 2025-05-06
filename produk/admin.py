680fafd5dd7340e17e033a098641bfc822dbf1c0
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-05-06 07:05:25 +0700

    feat: add Satuan model and integrate with Produk API

--- produk/admin.py
+++ produk/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import KategoriProduk, Produk
+from .models import KategoriProduk, Produk, Satuan
 
 # Register your models here.
 admin.site.register(KategoriProduk)
 admin.site.register(Produk)
+admin.site.register(Satuan)  # Register the new model
