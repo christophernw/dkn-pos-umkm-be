fc8e89ea55c49ac8f965af91851ebb70c0ba34d0
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-05-21 17:43:47 +0700

    debug: print current environment in settings

--- backend/settings.py
+++ backend/settings.py
@@ -140,6 +140,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Load environment variables
 ENV = os.environ.get('ENV', 'local')
 
+print(f"Environment: {ENV} cobaaaaaaa")
+
 # Database configuration
 if ENV == 'staging' or ENV == 'production':
     DATABASES = {
